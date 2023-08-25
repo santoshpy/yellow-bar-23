fb831e5f0a7c84bfb34d13c55e7e331f4b16fbd3
Santosh Purbey <purbey.santosh@hotmail.com>
2023-08-25 19:08:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-bar-23",
+  url: "https://yellow-bar-23.botics.co",
+  api: "https://yellow-bar-23.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
